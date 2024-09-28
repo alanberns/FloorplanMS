8625eb82f4743a278d4bf134a0c0eb17f62fd9e3
Alan Berns <alan_berns@yahoo.com.ar>
2024-09-27 22:14:32 -0300

    mongoose añadido

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://floorplan_db/nest')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
 export class AppModule {}
