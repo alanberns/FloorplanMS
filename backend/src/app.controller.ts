fbbcf05f0e6c0a771893ac1f4b487506165da1ac
Alan Berns <alan_berns@yahoo.com.ar>
2024-11-24 15:06:13 -0300

    nginx y 3 nodos

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as os from 'os';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/whoami')
+  getWhoAmI(): string {
+    const hostname = os.hostname();
+    return `You are being served by: ${hostname}`;
   }
 }
